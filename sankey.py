4e43aa06ec3c5b800d0b272ef015853907707be2
Anna Elise Wong <60045422+annaelisew@users.noreply.github.com>
2021-09-06 19:37:25 -0700

    Add files via upload

--- sankey.py
+++ sankey.py
@@ -1,3 +1,6 @@
+#Sankey Diagram python script for UX of AI research project
+#https://www.linkedin.com/pulse/guide-making-sankey-diagrams-using-python-latest-covid-ying-li/
+
 import pandas as pd
 import holoviews as hv
 hv.extension('bokeh')
@@ -5,27 +8,26 @@ from bokeh.plotting import show
 import csv
 
 #read the data
+file_name = "taxonomy_data.csv" #to run a new csv file, change this name
 col_list = ["Col1", "Col2", "Value"]
-csvdata = csv.reader(open("taxonomy_data.csv", "rt"), delimiter=",", quotechar="|")
+csvdata = csv.reader(open(file_name, "rt"), delimiter=",", quotechar="|")
 value_list = []
 
+#create array of all elements in Col2 (easier than listing out)
 for row in csvdata:
 	value_list.append(row[1])
 data = pd.read_csv("taxonomy_data.csv", usecols=col_list, error_bad_lines=False)
-print(data["Col1"])
 
-
+#format data
 boolean_series = data.Col2.isin(value_list)
-
 data = data[boolean_series]
-
 print (data.head)
 
-
+#make basic sankey diagram
 sankey1 = hv.Sankey(data,kdims=["Col1", "Col2"], vdims=["Value"])
 show (hv.render(sankey1))
 
-
+#more formatted version of sankey diagram
 sankey2 = hv.Sankey(data,kdims=["Col1", "Col2"], vdims=["Value"])
 sankey2.opts(cmap="PuBuGn_r",label_position='outer',
                                 edge_color='Col1', edge_line_width=0,
